72270ecb04cf0150a9e7fef64b40b7c4400eee72
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2023-09-28 18:59:21 +0530

    Time: 133 ms (67.34%), Space: 110.2 MB (8.26%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    double slidingwindowapproach(vector<int>& nums, int& k)
+    {
+        int i=0,j=k-1;
+        int sum=0;
+        for(int y=i;y<=j;y++)
+            sum += nums[y];
+            int maxsum=sum;
+            j++;
+        while(j < nums.size()){
+            sum -= nums[i++];
+            sum += nums[j++];
+            maxsum=max(maxsum,sum);
+        }
+        double maxavg=maxsum / (double)k;
+        return maxavg;
+    }
+
+    double findMaxAverage(vector<int>& nums, int k) {
+        return slidingwindowapproach(nums,k);
+    }
+};
