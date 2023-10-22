498d054b33a22263f6b4304d388bd08f69bf6a48
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2023-10-22 12:55:20 +0530

    Time: 0 ms (100%), Space: 6.6 MB (58.58%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    //comparator
+    static string str;
+    static bool compare(char ch1,char ch2)
+    {
+        return(str.find(ch1) < str.find(ch2));
+    }
+
+    string customSortString(string order, string s) {
+        str=order;
+        sort(s.begin(),s.end(),compare);
+        return s;
+    }
+};
+string Solution::str;
