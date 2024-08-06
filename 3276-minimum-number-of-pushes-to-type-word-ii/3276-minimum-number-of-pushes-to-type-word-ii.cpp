012df4a0a98bfa81d880c0e027bfe299a24cab7a
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2024-08-07 01:19:11 +0530

    Time: 71 ms (66.18%), Space: 27.6 MB (20.07%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -9,10 +9,26 @@ public:
 
         sort(freq.rbegin(),freq.rend()); //sort in descending order
 
-        int totalpush=0;
+        int totalpush=0,m=0;
         for(int i=0;i<26;i++)
         {
-            totalpush=totalpush+(i/8 + 1) * freq[i];
+            if(i>=0 && i<8)
+            {
+                m=1;
+            }
+            else if(i>=8 && i<16)
+            {
+                m=2;
+            }
+            else if(i>=16 && i<24)
+            {
+                m=3;
+            }
+            else
+            {
+                m=4;
+            }
+            totalpush += m*freq[i];
         }
         return totalpush;
     }
