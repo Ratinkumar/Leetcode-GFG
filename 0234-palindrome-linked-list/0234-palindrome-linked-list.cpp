d6a29cb5d51dcaace8ee44f9d80c29f472c0a444
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2024-09-01 23:54:08 +0530

    Time: 180 ms (36.39%), Space: 116.7 MB (92.25%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLL(ListNode* prev,ListNode* curr)
+    {
+        if(curr==NULL)
+        {
+            return prev;
+        }
+
+        ListNode* nextNode=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nextNode;
+
+        return reverseLL(prev,curr);
+    }
+
+    ListNode* middleNode(ListNode* head)
+    {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast->next!=NULL)
+        {
+            fast=fast->next;
+            if(fast->next!=NULL)
+            {
+                fast=fast->next;
+                slow=slow->next;
+            }
+        }
+        return slow;
+    }
+
+    bool compareLL(ListNode* head,ListNode* head2)
+    {
+        while(head!=NULL && head2!=NULL)
+        {
+            if(head->val != head2->val)
+            {
+                return false;
+            }
+            else
+            {
+                head=head->next;
+                head2=head2->next;
+            }
+        }
+        return true;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        //break into 2 halves
+        ListNode* midNode=middleNode(head);
+        ListNode* head2=midNode->next;
+        midNode->next=NULL;
+        //reverse second half
+        ListNode* prev=NULL;
+        ListNode* curr=head2;
+        head2=reverseLL(prev,curr);
+
+        //compare both linked list
+        bool ans=compareLL(head,head2);
+        return ans;
+    }
+};
