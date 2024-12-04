96f551dbfd7c204f10af893f7a1b833f90b76ac9
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2024-12-04 09:28:26 +0530

    Time: 0 ms (100%), Space: 10.7 MB (6.56%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    int UsingRecursion(vector<int>&nums,int index)
+    {
+        if(index>=nums.size()) return 0;
+
+        int include=nums[index]+UsingRecursion(nums,index+2);
+        int exclude=0+UsingRecursion(nums,index+1);
+
+        return max(include,exclude);
+    }
+
+    //Using DP
+    int UsingMemo(vector<int>& nums,int index,vector<int>&dp)
+    {
+        if(index>=nums.size()) return 0;
+
+        if(dp[index]!=-1) return dp[index];
+
+        int include=nums[index]+UsingMemo(nums,index+2,dp);
+        int exclude=0+UsingMemo(nums,index+1,dp);
+
+        dp[index]=max(include,exclude);
+
+        return dp[index];
+    }
+
+    int rob(vector<int>& nums) {
+        int index=0;
+        vector<int>dp(nums.size()+1,-1);
+        int ans=UsingMemo(nums,index,dp);
+        return ans;
+    }
+};
