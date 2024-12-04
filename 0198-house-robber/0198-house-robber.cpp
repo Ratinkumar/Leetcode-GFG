bf4a7477ded86af9140b57010252b8a2d7a44d52
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2024-12-04 10:05:58 +0530

    Time: 0 ms (100%), Space: 10.6 MB (8.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -27,17 +27,35 @@ public:
     }
 
     //Using Tabulation Method
-    // int usingTabu(vector<int>&nums,int index,vector<int>&dp)
-    // {
-    //     if(index>=nums.size()) return 0;
+    int UsingTabu(vector<int>&nums,int index,vector<int>&dp)
+    {
+        int n=nums.size();
+
+        dp[n-1]=nums[n-1];
+
+        for(int i=n-2;i>=0;i--)
+        {
+            int include=nums[i]+dp[i+2];
+            int exclude=0+dp[i+1];
+
+            dp[i]=max(include,exclude);
+        }
+
+        return dp[index];
 
 
-    // }
+
+    }
 
     int rob(vector<int>& nums) {
         int index=0;
-        vector<int>dp(nums.size(),-1);
-        int ans=UsingMemo(nums,index,dp);
+        int n=nums.size();
+        // vector<int>dp(n,-1);
+        // int ans=UsingMemo(nums,index,dp);
+        // return ans;
+        
+        vector<int>dp(n+1,0);
+        int ans=UsingTabu(nums,index,dp);
         return ans;
     }
 };
