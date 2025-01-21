8bee8ac6c635594d4c18caaec2e09880c6781960
Ratin Kumar <122297023+Ratinkumar@users.noreply.github.com>
2025-01-21 19:47:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (32.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,6 +5,7 @@ public:
         return n;
 
         vector<int>dp(n+1,-1);
+
         dp[0]=1;
         dp[1]=1;
 
@@ -12,6 +13,7 @@ public:
         {
             dp[i]=dp[i-1]+dp[i-2];
         }
+
         return dp[n];
     }
 };
